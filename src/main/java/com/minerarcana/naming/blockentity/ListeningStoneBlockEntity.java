fbebb6912a860f27b272e20991e14d1248a6bea8
Skylar <skylar.som@gmail.com>
2021-10-19 22:17:36 -0400

    Move some stuff out of Listening Stone

--- src/main/java/com/minerarcana/naming/blockentity/ListeningStoneBlockEntity.java
+++ src/main/java/com/minerarcana/naming/blockentity/ListeningStoneBlockEntity.java
@@ -4,21 +4,15 @@ import com.minerarcana.naming.block.ListeningStoneBlock;
 import com.minerarcana.naming.container.ListeningStoneContainer;
 import com.minerarcana.naming.content.NamingBlocks;
 import com.minerarcana.naming.worlddata.ListeningWorldData;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
-import net.minecraft.item.DyeColor;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.nbt.StringNBT;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SUpdateTileEntityPacket;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
-import net.minecraft.util.IReorderingProcessor;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
@@ -31,21 +25,13 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.function.Function;
 
-public class ListeningStoneBlockEntity extends TileEntity implements ISideText, INamedContainerProvider, Function<String, ListeningType> {
-    private final ITextComponent[] messages = new ITextComponent[]{
-            StringTextComponent.EMPTY,
-            StringTextComponent.EMPTY,
-            StringTextComponent.EMPTY,
-            StringTextComponent.EMPTY
-    };
+public class ListeningStoneBlockEntity extends MessageBlockEntity implements INamedContainerProvider, Function<String, ListeningType> {
     private final ListeningType[] listeningTypes = new ListeningType[]{
             ListeningType.NONE,
             ListeningType.NONE,
             ListeningType.NONE,
             ListeningType.NONE
     };
-    private final IReorderingProcessor[] renderedMessages = new IReorderingProcessor[4];
-    private String name = "";
 
     public ListeningStoneBlockEntity(TileEntityType<ListeningStoneBlockEntity> blockEntityType) {
         super(blockEntityType);
@@ -57,36 +43,20 @@ public class ListeningStoneBlockEntity extends TileEntity implements ISideText, 
     }
 
     @Override
-    public DyeColor getColor() {
-        return DyeColor.WHITE;
-    }
-
-    @Override
-    public IReorderingProcessor getRenderedMessage(int index, Function<ITextComponent, IReorderingProcessor> generate) {
-        if (renderedMessages[index] == null) {
-            renderedMessages[index] = generate.apply(messages[index]);
-        }
-        return renderedMessages[index];
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         if (this.getLevel() instanceof ServerWorld) {
             ListeningWorldData listeningWorldData = ((ServerWorld) this.getLevel()).getDataStorage()
                     .computeIfAbsent(ListeningWorldData::new, "spoken");
-            listeningWorldData.removeListener(this.name, this.worldPosition);
-            listeningWorldData.addListener(name, this.worldPosition, this);
+            listeningWorldData.removeListener(this.getName(), this.getBlockPos());
+            listeningWorldData.addListener(name, this.getBlockPos(), this);
         }
-        this.name = name;
+        super.setName(name);
     }
 
     public ListeningType apply(String spoken) {
         ListeningType listeningType = ListeningType.NONE;
         for (int i = 0; i < 4; i++) {
-            if (spoken.equalsIgnoreCase(messages[i].getContents())) {
+            if (spoken.equalsIgnoreCase(this.getMessage(i).getContents())) {
                 listeningType = listeningType.ordinal() > listeningTypes[i].ordinal() ? listeningType : listeningTypes[i];
             }
         }
@@ -107,7 +77,7 @@ public class ListeningStoneBlockEntity extends TileEntity implements ISideText, 
         if (this.getLevel() instanceof ServerWorld) {
             ListeningWorldData listeningWorldData = ((ServerWorld) this.getLevel()).getDataStorage()
                     .computeIfAbsent(ListeningWorldData::new, "spoken");
-            listeningWorldData.addListener(this.name, this.worldPosition, this);
+            listeningWorldData.addListener(this.getName(), this.worldPosition, this);
         }
     }
 
@@ -119,29 +89,10 @@ public class ListeningStoneBlockEntity extends TileEntity implements ISideText, 
         listeningTypes[index] = listeningType;
     }
 
-    public ITextComponent getMessage(int index) {
-        return messages[index];
-    }
-
-    public void setMessage(int index, ITextComponent message) {
-        messages[index] = message;
-        renderedMessages[index] = null;
-        if (this.getLevel() != null) {
-            this.getLevel().markAndNotifyBlock(
-                    this.getBlockPos(),
-                    this.getLevel().getChunkAt(this.getBlockPos()),
-                    this.getBlockState(),
-                    this.getBlockState(),
-                    Constants.BlockFlags.DEFAULT,
-                    512
-            );
-        }
-    }
-
     @Override
     @Nonnull
     public ITextComponent getDisplayName() {
-        return new StringTextComponent(this.name);
+        return new StringTextComponent(this.getName());
     }
 
     @Nullable
@@ -155,66 +106,22 @@ public class ListeningStoneBlockEntity extends TileEntity implements ISideText, 
     }
 
     @Override
-    @ParametersAreNonnullByDefault
-    public void load(BlockState state, CompoundNBT nbt) {
-        super.load(state, nbt);
-        loadMessages(nbt);
-    }
-
-    private void loadMessages(CompoundNBT nbt) {
-        this.name = nbt.getString("name");
-        ListNBT messagesNBT = nbt.getList("messages", Constants.NBT.TAG_STRING);
-        for (int i = 0; i < messagesNBT.size(); i++) {
-            this.messages[i] = ITextComponent.Serializer.fromJson(messagesNBT.getString(i));
-            this.renderedMessages[i] = null;
-        }
+    protected void loadMessages(CompoundNBT nbt) {
+        super.loadMessages(nbt);
         ListNBT listeningTypesNBT = nbt.getList("listeningTypes", Constants.NBT.TAG_STRING);
         for (int i = 0; i < listeningTypesNBT.size(); i++) {
             this.listeningTypes[i] = ListeningType.valueOf(listeningTypesNBT.getString(i));
         }
     }
 
-    @Override
     @Nonnull
-    public CompoundNBT save(@Nonnull CompoundNBT compoundNBT) {
-        return saveMessages(super.save(compoundNBT));
-    }
-
-    @Nonnull
-    private CompoundNBT saveMessages(@Nonnull CompoundNBT nbt) {
-        nbt.putString("name", name);
-        ListNBT messagesNBT = new ListNBT();
-        for (ITextComponent message : messages) {
-            messagesNBT.add(StringNBT.valueOf(ITextComponent.Serializer.toJson(message)));
-        }
-        nbt.put("messages", messagesNBT);
+    protected CompoundNBT saveMessages(@Nonnull CompoundNBT nbt) {
+        nbt = super.saveMessages(nbt);
         ListNBT listeningTypeNBT = new ListNBT();
         for (ListeningType listeningType : listeningTypes) {
             listeningTypeNBT.add(StringNBT.valueOf(listeningType.name()));
         }
         nbt.put("listeningTypes", listeningTypeNBT);
         return nbt;
-    }
-
-    @Override
-    public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
-        this.loadMessages(pkt.getTag());
-    }
-
-    @Nullable
-    @Override
-    public SUpdateTileEntityPacket getUpdatePacket() {
-        return new SUpdateTileEntityPacket(this.getBlockPos(), -1, saveMessages(new CompoundNBT()));
-    }
-
-    @Override
-    public void handleUpdateTag(BlockState state, CompoundNBT tag) {
-        this.loadMessages(tag);
-    }
-
-    @Override
-    @Nonnull
-    public CompoundNBT getUpdateTag() {
-        return save(new CompoundNBT());
     }
 }
---
+++ src/main/java/com/minerarcana/naming/blockentity/MessageBlockEntity.java
@@ -0,0 +1,132 @@
+package com.minerarcana.naming.blockentity;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.item.DyeColor;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.StringNBT;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.IReorderingProcessor;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.common.util.Constants;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.Function;
+
+public abstract class MessageBlockEntity extends TileEntity implements ISideText {
+    private final ITextComponent[] messages = new ITextComponent[]{
+            StringTextComponent.EMPTY,
+            StringTextComponent.EMPTY,
+            StringTextComponent.EMPTY,
+            StringTextComponent.EMPTY
+    };
+    private final IReorderingProcessor[] renderedMessages = new IReorderingProcessor[4];
+
+    private String name = "";
+
+    public MessageBlockEntity(TileEntityType<?> blockEntityType) {
+        super(blockEntityType);
+    }
+
+    @Override
+    public DyeColor getColor() {
+        return DyeColor.WHITE;
+    }
+
+    @Override
+    public IReorderingProcessor getRenderedMessage(int index, Function<ITextComponent, IReorderingProcessor> generate) {
+        if (renderedMessages[index] == null) {
+            renderedMessages[index] = generate.apply(messages[index]);
+        }
+        return renderedMessages[index];
+    }
+
+    public ITextComponent getMessage(int index) {
+        return messages[index];
+    }
+
+    public void setMessage(int index, ITextComponent message) {
+        messages[index] = message;
+        renderedMessages[index] = null;
+        if (this.getLevel() != null) {
+            this.getLevel().markAndNotifyBlock(
+                    this.getBlockPos(),
+                    this.getLevel().getChunkAt(this.getBlockPos()),
+                    this.getBlockState(),
+                    this.getBlockState(),
+                    Constants.BlockFlags.DEFAULT,
+                    512
+            );
+        }
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void load(BlockState state, CompoundNBT nbt) {
+        super.load(state, nbt);
+        loadMessages(nbt);
+    }
+
+    protected void loadMessages(CompoundNBT nbt) {
+        this.name = nbt.getString("name");
+        ListNBT messagesNBT = nbt.getList("messages", Constants.NBT.TAG_STRING);
+        for (int i = 0; i < messagesNBT.size(); i++) {
+            this.messages[i] = ITextComponent.Serializer.fromJson(messagesNBT.getString(i));
+            this.renderedMessages[i] = null;
+        }
+    }
+
+    @Override
+    @Nonnull
+    public CompoundNBT save(@Nonnull CompoundNBT compoundNBT) {
+        return saveMessages(super.save(compoundNBT));
+    }
+
+    @Nonnull
+    protected CompoundNBT saveMessages(@Nonnull CompoundNBT nbt) {
+        nbt.putString("name", name);
+        ListNBT messagesNBT = new ListNBT();
+        for (ITextComponent message : messages) {
+            messagesNBT.add(StringNBT.valueOf(ITextComponent.Serializer.toJson(message)));
+        }
+        nbt.put("messages", messagesNBT);
+        return nbt;
+    }
+
+
+    @Override
+    public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+        this.loadMessages(pkt.getTag());
+    }
+
+    @Nullable
+    @Override
+    public SUpdateTileEntityPacket getUpdatePacket() {
+        return new SUpdateTileEntityPacket(this.getBlockPos(), -1, saveMessages(new CompoundNBT()));
+    }
+
+    @Override
+    public void handleUpdateTag(BlockState state, CompoundNBT tag) {
+        this.loadMessages(tag);
+    }
+
+    @Override
+    @Nonnull
+    public CompoundNBT getUpdateTag() {
+        return save(new CompoundNBT());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
