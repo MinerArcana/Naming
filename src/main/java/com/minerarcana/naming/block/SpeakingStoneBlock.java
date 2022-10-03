4f73ce98b75f9abe28dbaa5a32b3bce9f1ada5c9
Skylar <skylar.som@gmail.com>
2022-10-02 23:01:14 -0400

    Update version

--- src/main/java/com/minerarcana/naming/block/SpeakingStoneBlock.java
+++ src/main/java/com/minerarcana/naming/block/SpeakingStoneBlock.java
@@ -11,7 +11,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
